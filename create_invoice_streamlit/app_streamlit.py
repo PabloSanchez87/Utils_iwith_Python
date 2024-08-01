a36ba083e1f8384ed97da8cab6539d70bd36298c
PabloSanchez <sancheztorrespablo@gmail.com>
2024-08-01 17:36:51 +0200

    Comentamos el código

--- create_invoice_streamlit/app_streamlit.py
+++ create_invoice_streamlit/app_streamlit.py
@@ -2,55 +2,55 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 import cloudinary.uploader
-from config import cloudinary, set_page_config, euro_symbol, total_expenses, final_price
+from config import cloudinary, set_page_config, euro_symbol
 import io
 from create_invoice import process_invoice
 
 #------------ CONFIGURACION STREAMLIT ------------
-set_page_config()
+set_page_config()  # Configuración inicial de la página, como título y layout
 
 #-------------- VARIABLES DE ESTADO --------------
-if "first_time" not in st.session_state:
-    st.session_state.first_time = ""  # Marca si es la primera vez que se accede
-if "items_invoice" not in st.session_state:
-    st.session_state.items_invoice = []  # Lista de artículos para la factura
+# Estas variables de estado persisten a lo largo de la sesión del usuario
 if "expense_data" not in st.session_state:
-    st.session_state.expense_data = []  # Lista para almacenar los datos de los gastos
+    st.session_state.expense_data = []  # Lista para almacenar los artículos añadidos
 if "invoice_data" not in st.session_state:
-    st.session_state.invoice_data = []  # Inicializar la lista para almacenar datos de los artículos de la factura
+    st.session_state.invoice_data = []  # Lista para almacenar los datos procesados para la factura
 
 #-------------- CÓDIGO DE INTERFAZ ----------------
-# Título
+# Título principal de la aplicación
 st.markdown("<h1 style='text-align: center; color: red;'>Generador de facturas</h1>", unsafe_allow_html=True)
 
 # Sección de información de la factura
 with st.container():
     cc1, ccaux, cc2 = st.columns([2, 0.1, 2])
     cc1.subheader("Datos")
-    from_who = cc1.text_area("De: *", placeholder="Nombre completo:\nDirección:\nTeléfono:\nCIF o DNI:\n", height=110)  # Campo para el remitente de la factura
-    to_who = cc1.text_area("Para: *", placeholder="Nombre completo:\nDirección:\nTeléfono:\nCIF o DNI:\n", height=110)  # Campo para el destinatario de la factura
+    
+    # Datos del remitente de la factura (quien emite)
+    from_who = cc1.text_area("De: *", placeholder="Nombre completo:\nDirección:\nTeléfono:\nCIF o DNI:\n", height=110)
+    
+    # Datos del destinatario de la factura (quien recibe)
+    to_who = cc1.text_area("Para: *", placeholder="Nombre completo:\nDirección:\nTeléfono:\nCIF o DNI:\n", height=110)
     
     cc2.subheader("Factura")
-    # Fila interna dentro de cc2 para número de factura, fecha y fecha de vencimiento
+    # Campos para el número de factura, fecha y fecha de vencimiento
     with cc2:
         num_invoice_col, date_invoice_col, due_date_col = st.columns([1, 1, 1])
-        num_invoice = num_invoice_col.text_input("Número de factura (Personalizable) *", placeholder="Número de factura")  # Campo para el número de factura
-        date_invoice = date_invoice_col.date_input("Fecha *")  # Campo para la fecha de la factura
-        due_date = due_date_col.date_input("Fecha de vencimiento *")  # Campo para la fecha de vencimiento
+        num_invoice = num_invoice_col.text_input("Número de factura (Personalizable) *", placeholder="Número de factura")
+        date_invoice = date_invoice_col.date_input("Fecha *")
+        due_date = due_date_col.date_input("Fecha de vencimiento *")
     
-    # Subida de archivo
+    # Opción para subir un logo a la factura
     uploaded_file = cc2.file_uploader("Cargar un logo", type=["jpg", "jpeg", "png"])
     url_logo = None
     if uploaded_file is not None:
         try:
-            # Abre la imagen
+            # Cargar y preparar la imagen para subida
             image = Image.open(uploaded_file)
-            # Convertir la imagen a bytes para subirla
             image_bytes = io.BytesIO()
             image.save(image_bytes, format='PNG')
             image_bytes = image_bytes.getvalue()
 
-            # Subir la imagen a Cloudinary
+            # Subir la imagen a Cloudinary y obtener la URL
             response = cloudinary.uploader.upload(io.BytesIO(image_bytes), folder="logos/")
             url_logo = response['url']
             cc2.success("Logo cargado exitosamente.")
@@ -60,19 +60,23 @@ with st.container():
 # Formulario para añadir artículos a la factura
 with st.form("entry_form", clear_on_submit=True):
     cex1, cex2, cex3 = st.columns([6, 0.5, 0.5])
-    articulo = cex1.text_input("Artículo", placeholder="Descripción del servicio o producto")  # Descripción del artículo o servicio
-    cantidad = cex2.number_input("Cantidad", step=1, min_value=1)  # Cantidad del artículo o servicio
-    precio_unit = cex3.number_input("Precio unitario", min_value=0.0, format="%.2f")  # Precio del artículo o servicio
     
-    submitted_expense = st.form_submit_button("Añadir artículo")  # Botón para añadir el artículo
+    # Campos para ingresar los detalles de cada artículo
+    articulo = cex1.text_input("Artículo", placeholder="Descripción del servicio o producto")
+    cantidad = cex2.number_input("Cantidad", step=1, min_value=1)
+    precio_unit = cex3.number_input("Precio unitario", min_value=0.0, format="%.2f")
+    
+    # Botón para añadir el artículo
+    submitted_expense = st.form_submit_button("Añadir artículo")
     
     if submitted_expense:
         if articulo == "":
             st.warning("Añade una descripción del artículo o servicio")
         else:
-            # Calcular subtotal sin descuento
+            # Calcular el subtotal para el artículo añadido
             subtotal = precio_unit * cantidad
             st.success("Artículo añadido")
+            # Guardar los datos del artículo en el estado de sesión
             st.session_state.expense_data.append({
                 "name": articulo, 
                 "quantity": cantidad,  
@@ -82,10 +86,10 @@ with st.form("entry_form", clear_on_submit=True):
 
 # Uso de st.data_editor para edición interactiva de la tabla de artículos
 if st.session_state.expense_data:
-    df_expense = pd.DataFrame(st.session_state.expense_data)  # Convertir datos de gastos a DataFrame de pandas
+    df_expense = pd.DataFrame(st.session_state.expense_data)  # Convertir los datos a un DataFrame de pandas
     st.subheader("Artículos añadidos")
     
-    # Usar st.data_editor para editar la tabla
+    # Editor interactivo para modificar los artículos añadidos
     edited_df = st.data_editor(
         df_expense,
         num_rows="dynamic",
@@ -115,14 +119,13 @@ if st.session_state.expense_data:
     if not deleted_rows.empty:
         st.success(f"{len(deleted_rows)} filas eliminadas exitosamente.")
         
-    # Actualizar el DataFrame y el estado de sesión si hubo cambios
+    # Actualizar el DataFrame y el estado de sesión con los datos editados
     st.session_state.expense_data = edited_df.to_dict('records')
     # Crear una versión sin el campo 'subtotal' para pasar a la API
     invoice_data_clean = edited_df.drop(columns=["subtotal"]).to_dict('records')
     st.session_state.invoice_data = invoice_data_clean  # Actualizar los datos de la factura
-    total_expenses = edited_df["subtotal"].sum()  # Calcular el total de gastos
-    st.text(f"Total: {total_expenses:.2f} {euro_symbol}")
-    final_price = total_expenses
+    total_expenses = edited_df["subtotal"].sum()  # Calcular el total de gastos (subtotal general)
+    st.text(f"Subtotal: {total_expenses:.2f} {euro_symbol}")
 
 # Sección de información adicional de la factura
 with st.container():
@@ -135,17 +138,19 @@ with st.container():
     cc5, cc6, cc7, ccaux  = st.columns([2, 1.5, 4, 9])  # Ajusta el tamaño de las columnas según sea necesario
     descuento = cc5.number_input("Descuento sobre el total %: ", min_value=0, max_value=100, step=1, format="%d")  # Campo para el porcentaje de descuento
     if descuento:
-        final_price = round(final_price - ((descuento / 100) * final_price), 2)
+        total_expenses = round(total_expenses - ((descuento / 100) * total_expenses), 2)
     impuesto = cc6.number_input("Impuestos %: ", min_value=0, max_value=100, step=1, format="%d")  # Campo para el porcentaje de impuesto
     
     # Calcular el precio después de aplicar impuestos
+    final_price = total_expenses
     if impuesto:
-        final_price = final_price * (1 + (impuesto / 100))
+        final_price = round(total_expenses * (1 + (impuesto / 100)), 2)
     cc5.write(f"Subtotal: {total_expenses:.2f} {euro_symbol}")
     cc6.write(f":red[Total: {final_price:.2f} {euro_symbol}]")
 
 # Botón para enviar y generar la factura
 submit = st.button("Enviar", type="primary") 
 
+# Llamada a la función de procesamiento de la factura al enviar
 if submit:
     process_invoice(from_who, to_who, num_invoice, date_invoice, due_date, url_logo, st.session_state.invoice_data, impuesto, descuento, notes, term)
--- create_invoice_streamlit/create_invoice.py
+++ create_invoice_streamlit/create_invoice.py
@@ -4,28 +4,41 @@ from connect_to_api import ApiConnector
 import streamlit as st
 
 def process_invoice(from_who, to_who, num_invoice, date_invoice, due_date, url_logo, items_invoice, impuesto, descuento, notes, term):
+    # Validar que los campos obligatorios estén llenos
     if not from_who or not to_who or not num_invoice or not date_invoice or not due_date:
         st.warning("Completa los campos obligatorios")
     elif not items_invoice:
         st.warning("Añade algún artículo")
     else:
         try:
-            # Generar factura en PDF
-            api = ApiConnector()  # Crear instancia de ApiConnector
-            root_invoice = api.connect_api_and_save_invoice_pdf(from_who, to_who, url_logo, num_invoice, str(date_invoice), str(due_date), items_invoice, impuesto, descuento, notes, term)
+            # Crear una instancia de ApiConnector para interactuar con la API
+            api = ApiConnector()
+            
+            # Llamada a la API para generar la factura y obtener el path del archivo PDF
+            root_invoice = api.connect_api_and_save_invoice_pdf(
+                from_who, to_who, url_logo, num_invoice,
+                str(date_invoice), str(due_date), items_invoice,
+                impuesto, descuento, notes, term
+            )
 
+            # Leer el archivo PDF generado
             with open(root_invoice, "rb") as file:
                 pdf_data = file.read()
+                
             st.success("Desde aquí puedes descargar la factura generada")
             
-            # Norlmalizamos el nombre con la primera palabra del to_who
+            # Extraer la primera línea de 'to_who' para usar como parte del nombre del archivo
             to_first_line = to_who.split('\n', 1)[0].strip() if to_who else "SinNombre"
-            # Asegurar un nombre de archivo seguro y limpio
-            to_first_line = re.sub(r'[^\w\s-]', '', to_first_line)  # Eliminar caracteres no deseados
+            
+            # Limpiar el nombre de archivo de caracteres no deseados para asegurarse de que sea seguro
+            to_first_line = re.sub(r'[^\w\s-]', '', to_first_line)
             filename = f"Factura_{to_first_line}_{num_invoice}.pdf"
             
+            # Proporcionar un botón de descarga del PDF generado
             st.download_button(label="Descargar factura", data=pdf_data, file_name=filename, mime="application/pdf")
-            os.remove(root_invoice)  # Eliminar el archivo PDF temporal después de la descarga
+            
+            # Eliminar el archivo PDF temporal después de la descarga para liberar espacio
+            os.remove(root_invoice)
             
         except Exception as excep:
             st.warning(f"Hubo un problema generando la factura pdf: {str(excep)}")
